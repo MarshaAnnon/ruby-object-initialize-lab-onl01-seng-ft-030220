703356ec8a1918908bd894bacc0e384900c3100b
Marsha Annon <marshaannon@gmail.com>
2020-03-10 14:32:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,8 @@
 class Dog
 
-  def initialize(name, breed)
+  def initialize(name, breed="Mutt")
     @name = name
-    @breed.each do |breed|
-      if breed = breed
-        puts @breed
-
-      else
-        "Mutt"
+    @breed = breed
   end
 
   def name=(name)
